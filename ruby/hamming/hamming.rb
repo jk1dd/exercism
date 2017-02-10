91c1573ed8a69c5082d565912e426b721f389f18
jk1dd <portajonk@gmail.com>
2017-02-10 11:25:16 -0700

    submit hamming exercism

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,6 +1,16 @@
 class Hamming
 
-  def compute
+  def self.compute(strand_1, strand_2)
+    if strand_1.length != strand_2.length
+      raise ArgumentError
+    else
+      strand_1.chars.zip(strand_2.chars).count {|f, s| f != s}
+    end
   end
-  
+
+end
+
+
+module BookKeeping
+  VERSION = 3 # Where the version number matches the one in the test.
 end
