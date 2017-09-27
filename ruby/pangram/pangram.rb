f3edfb3b93e6ba66c975c6967f142023fd9e5657
jk1dd <portajonk@gmail.com>
2017-09-27 15:00:27 -0400

    add solution for pangram, pull down sieve

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,3 +1,13 @@
+require 'pry'
 class Pangram
-  
+
+  def self.pangram?(phrase)
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    letters = phrase.downcase.chars.delete_if {|char| !('a'..'z').include?(char) }.sort.uniq.join
+    letters == alphabet ? true : false
+  end
+end
+
+module BookKeeping
+  VERSION = 4 # Where the version number matches the one in the test.
 end
