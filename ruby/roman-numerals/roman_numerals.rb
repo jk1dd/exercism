54fa7dcf871d702b03bd9c66df5aee4f76507258
jk1dd <portajonk@gmail.com>
2017-10-04 15:45:10 -0400

    add solution to roman numeral

---
+++ ruby/roman-numerals/roman_numerals.rb
@@ -0,0 +1,28 @@
+# create map of numerals
+# convert and subtract
+
+class Fixnum
+  # I, V, X, L, C, D, M.
+  NUM_MAP = [
+    ["I", 1], ["IV", 4], ["V", 5], ["IX", 9],
+    ["X", 10], ["XL", 40], ["L", 50], ["XC", 90],
+    ["C", 100], ["CD", 400], ["D", 500], ["CM", 900], ["M", 1000]
+  ]
+
+  def to_roman
+    result = ''
+    number = self
+    NUM_MAP.reverse.each do |pair|
+      while number >= pair[1]
+        result << pair[0]
+        number -= pair[1]
+      end
+    end
+    result
+  end
+
+end
+
+module BookKeeping
+  VERSION = 2
+end
